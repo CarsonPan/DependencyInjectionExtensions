9e3ca877ffd06df8c2e6f9a0286386187f803324
CarsonPan <carsonpan@outlook.com>
2019-05-09 20:21:01 +0800

    反射辅助类

---
+++ src/DuckGo.DependencyInjection/ReflectionHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DuckGo.DependencyInjection
+{
+    public class ReflectionHelper
+    {
+        public IEnumerable<Type> GetTypes(Assembly assembly,Type flagType)
+        {
+            return assembly.GetExportedTypes().Where(type => type.IsClass && //类
+                                                   !type.IsAbstract &&//非抽象
+                                                   !type.IsDefined(typeof(ComponentAttribute))&&//未标记ComponentAttribute 属性
+                                                   flagType.IsAssignableFrom(type)
+                                              );
+        }
+    }
+}
